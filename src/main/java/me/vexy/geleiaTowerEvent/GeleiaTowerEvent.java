1ff1bb29595543435ea7ec0eaec53789c373234d
Daniel <90870585+VexyPT@users.noreply.github.com>
2024-10-27 20:13:25 +0000

    initial commit

---
+++ src/main/java/me/vexy/geleiaTowerEvent/GeleiaTowerEvent.java
@@ -0,0 +1,13 @@
+package me.vexy.geleiaTowerEvent;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GeleiaTowerEvent extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
